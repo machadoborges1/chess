fe438a16e2e411ad13a9ff1376fdddd15f1189b6
Humberto <hummberton@gmail.com>
2023-03-07 14:58:48 -0300

    chess

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,12 @@
 package application;
 
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Main {
     public static void main(String[] args) {
 
-        Position pos = new Position(3,4);
-        System.out.println(pos);
+        ChessMatch chess = new ChessMatch();
+        UI.printBoard(chess.getPieces())
     }
 }
