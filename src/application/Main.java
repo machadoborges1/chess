c8084a686ba497b9138a3f799e92c52d479a60b2
Humberto <hummberton@gmail.com>
2023-03-07 15:19:30 -0300

    tabuleiro

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
 
         ChessMatch chess = new ChessMatch();
-        UI.printBoard(chess.getPieces())
+        UI.printBoard(chess.getPieces());
     }
 }
